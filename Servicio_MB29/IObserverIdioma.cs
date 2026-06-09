69bc193c7157e7f4d1a7c1a141a61792fabeee02
Bilek, Agustin <bilekagustin@gmail.com>
2026-06-09 17:18:06 -0300

    Agregar IObserverIdioma e ISujetoComponente

---
+++ Servicio_MB29/IObserverIdioma.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio_MB29
+{
+    public interface IObserverIdioma
+    {
+
+        void actualizar_MB29(string idioma);
+
+
+
+    }
+}
---
+++ Servicio_MB29/ISujetoComponente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicio_MB29
+{
+    public interface ISujetoComponente
+    {
+         void Agregar_MB29(IObserverIdioma idioma);
+
+         void Eliminar_MB29(IObserverIdioma idioma);
+
+        void Notificar_MB29();
+
+
+
+
+    }
+}
